1470577f742b43f0bf585ea536e09df5c2543204
JaquelineCamarillo <juanaolaez95@gmail.com>
2026-02-10 19:58:51 -0600

    fix: Uso de validaciones y CRUD funcional

--- src/modules/task/task.controller.ts
+++ src/modules/task/task.controller.ts
@@ -1,33 +1,35 @@
-import { Body, Controller, Delete, Param, Post, Put } from "@nestjs/common";
-import { Get } from "@nestjs/common";
-import { TaskService } from "./task.service";
+import { Body, Controller, Delete, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { Get } from '@nestjs/common';
+import { TaskService } from './task.service';
+import { CreateTaskDto } from './dto/create-task.dto';
 
-@Controller("api/tasks")
+@Controller('api/tasks')
 export class TaskController {
+  constructor(private taskSvc: TaskService) {}
 
-    constructor(private taskSvc: TaskService) {}
-        
-        @Get()
-        public getTasks(): string {
-            return this.taskSvc.getTasks();
-        }
-        @Get(":id")
-        public getTasksById(@Param("id") id: string): string {
-            return this.taskSvc.getTaskById(parseInt(id));
-        }
-        
-        @Post() 
-        public insertTask(@Body() task: any): string {
-            return this.taskSvc.insert(task);
-        }
+  @Get()
+  public getTasks(): any[] {
+    return this.taskSvc.getTasks();
+  }
 
-        @Put()
-        public updateTask(@Body("id") id:string,@Body() task:any ) {
-            return this.taskSvc.update(parseInt(id), task);
-        }
+  @Get(":id")
+  public getTasksById(@Param("id", ParseIntPipe) id: number): any {
+    console.log(typeof id);
+    return this.taskSvc.getTaskById(id);
+  }
 
-        @Delete(":id")
-        public deleteTask(@Param("id") id:string ) {
-            return this.taskSvc.delete(parseInt(id));
-        }
-} 
+  @Post()
+  public insertTask(@Body() task: CreateTaskDto): string {
+    return this.taskSvc.insert(task);
+  }
+
+  @Put("/:id")
+  public updateTask(@Param("id", ParseIntPipe) id: number, @Body() task: any) {
+    return this.taskSvc.update(id, task);
+  }
+
+  @Delete(":id")
+  public deleteTask(@Param(":id") id: string) {
+    return this.taskSvc.delete(parseInt(id));
+  }
+}
--- src/modules/task/task.service.ts
+++ src/modules/task/task.service.ts
@@ -1,25 +1,47 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable } from '@nestjs/common';
+import { CreateTaskDto } from './dto/create-task.dto';
 
 @Injectable()
 export class TaskService {
+  private tasks: any[] = [];
 
-    public getTasks() {
-        return "Tarea creada exitosamente";
-    }
+  public getTasks(): any[] {
+    return this.tasks;
+  }
 
-    public getTaskById(id: number): string {
-        return `Tarea con el id: ${id}`;
-    }
+  public getTaskById(id: number): string {
+    var task = this.tasks.find((t) => t.id === id);
+    return `Tarea con el id: ${id}`;
+  }
 
-    public insert (task: any): string {
-        return task;
-    }
+  public insert(task: CreateTaskDto): any {
+    var id = this.tasks.length + 1;
+    var insertedTask = this.tasks.push({
+      ...task,
+      id,
+    });
+    
+    return this.tasks[insertedTask - 1];
+  }
 
-    public update (id: number, task: any): string {
-        return task;
-    }
+  public update(id: number, task: any) {
+    const taskUpdate = this.tasks.map(t => {
+      if (t.id == id) {
 
-    public delete (id: number): string {
-        return `Tarea con el id: ${id} eliminada`;
-    }
+        if (task.name) t.name = task.name;
+        if (task.description) t.description = task.description;
+        if (task.priority) t.priority = task.priority;
+
+        return t;
+      }
+      return t;
+    });
+    return taskUpdate;
+  }
+
+  public delete(id: number): string {
+    const array = this.tasks.filter(t => t.id !== id);
+    this.tasks = array;
+    return "Tarea eliminada correctamente";
+  }
 }
